ad2528d5bf41d4cee7877069761771bb5b2cbd12
Tom ROBERTO <tomroberto@Air-de-Tom.home>
2021-05-07 18:12:25 +0200

    route offer and model Offer updated for multiple pictures

--- routes/offer.js
+++ routes/offer.js
@@ -34,6 +34,7 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
           { COULEUR: color },
           { EMPLACEMENT: city },
         ],
+        product_pictures: [],
         owner: req.user,
       });
       if (req.files.picture) {
@@ -42,6 +43,46 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
           { folder: `/vinted/offers/${newOffer.id}` }
         );
         newOffer.product_image = imgUploaded;
+        newOffer.product_pictures.push(imgUploaded);
+      }
+
+      if (req.files.picture2) {
+        const imgUploaded2 = await cloudinary.uploader.upload(
+          req.files.picture2.path,
+          { folder: `/vinted/offers/${newOffer.id}` }
+        );
+        newOffer.product_pictures.push(imgUploaded2);
+      }
+
+      if (req.files.picture3) {
+        const imgUploaded3 = await cloudinary.uploader.upload(
+          req.files.picture3.path,
+          { folder: `/vinted/offers/${newOffer.id}` }
+        );
+        newOffer.product_pictures.push(imgUploaded3);
+      }
+      if (req.files.picture4) {
+        const imgUploaded4 = await cloudinary.uploader.upload(
+          req.files.picture4.path,
+          { folder: `/vinted/offers/${newOffer.id}` }
+        );
+        newOffer.product_pictures.push(imgUploaded4);
+      }
+
+      if (req.files.picture5) {
+        const imgUploaded5 = await cloudinary.uploader.upload(
+          req.files.picture5.path,
+          { folder: `/vinted/offers/${newOffer.id}` }
+        );
+        newOffer.product_pictures.push(imgUploaded5);
+      }
+
+      if (req.files.picture6) {
+        const imgUploaded6 = await cloudinary.uploader.upload(
+          req.files.picture6.path,
+          { folder: `/vinted/offers/${newOffer.id}` }
+        );
+        newOffer.product_pictures.push(imgUploaded6);
       }
 
       await newOffer.save();
@@ -71,25 +112,145 @@ router.put("/offer/update", isAuthenticated, async (req, res) => {
   } = req.fields;
   try {
     const offer = await Offer.findById(_id);
-    await cloudinary.uploader.destroy(offer.product_image.public_id);
-    offer.product_name = title;
-    offer.product_description = description;
-    offer.product_price = price;
-    offer.product_details = [
-      { MARQUE: brand },
-      { TAILLE: size },
-      { ÉTAT: condition },
-      { COULEUR: color },
-      { EMPLACEMENT: city },
-    ];
-    offer.owner = req.user;
-    const imgUploaded = await cloudinary.uploader.upload(
-      req.files.picture.path,
-      {
-        folder: `/vinted/offers/${offer.id}`,
+    if (req.files.picture) {
+      await cloudinary.uploader.destroy(offer.product_image.public_id);
+      const imgUploaded = await cloudinary.uploader.upload(
+        req.files.picture.path,
+        {
+          folder: `/vinted/offers/${offer.id}`,
+        }
+      );
+      offer.product_image = imgUploaded;
+      offer.product_pictures[0] = imgUploaded;
+      offer.markModified("product_pictures");
+    }
+    if (title) {
+      offer.product_name = title;
+    }
+    if (description) {
+      offer.product_description = description;
+    }
+    if (price) {
+      offer.product_price = price;
+    }
+
+    const details = offer.product_details;
+    for (let i = 0; i < details.length; i++) {
+      if (details[i].MARQUE) {
+        if (brand) {
+          details[i].MARQUE = brand;
+        }
       }
-    );
-    offer.product_image = imgUploaded;
+      if (details[i].TAILLE) {
+        if (size) {
+          details[i].TAILLE = size;
+        }
+      }
+      if (details[i].ÉTAT) {
+        if (condition) {
+          details[i].ÉTAT = condition;
+        }
+      }
+      if (details[i].COULEUR) {
+        if (color) {
+          details[i].COULEUR = color;
+        }
+      }
+      if (details[i].EMPLACEMENT) {
+        if (city) {
+          details[i].EMPLACEMENT = city;
+        }
+      }
+    }
+    offer.markModified("product_details");
+
+    if (req.files.picture2) {
+      if (offer.product_pictures[1]) {
+        await cloudinary.uploader.destroy(offer.product_pictures[1].public_id);
+        const imgUploaded2 = await cloudinary.uploader.upload(
+          req.files.picture2.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures[1] = imgUploaded2;
+      } else {
+        const imgUploaded2 = await cloudinary.uploader.upload(
+          req.files.picture2.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures.push(imgUploaded2);
+      }
+    }
+
+    if (req.files.picture3) {
+      if (offer.product_pictures[2]) {
+        await cloudinary.uploader.destroy(offer.product_pictures[2].public_id);
+        const imgUploaded3 = await cloudinary.uploader.upload(
+          req.files.picture3.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures[2] = imgUploaded3;
+      } else {
+        const imgUploaded3 = await cloudinary.uploader.upload(
+          req.files.picture3.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures.push(imgUploaded3);
+      }
+    }
+
+    if (req.files.picture4) {
+      if (offer.product_pictures[3]) {
+        await cloudinary.uploader.destroy(offer.product_pictures[3].public_id);
+        const imgUploaded4 = await cloudinary.uploader.upload(
+          req.files.picture4.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures[3] = imgUploaded4;
+      } else {
+        const imgUploaded4 = await cloudinary.uploader.upload(
+          req.files.picture4.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures.push(imgUploaded4);
+      }
+    }
+
+    if (req.files.picture5) {
+      if (offer.product_pictures[4]) {
+        await cloudinary.uploader.destroy(offer.product_pictures[4].public_id);
+        const imgUploaded5 = await cloudinary.uploader.upload(
+          req.files.picture5.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures[4] = imgUploaded5;
+      } else {
+        const imgUploaded5 = await cloudinary.uploader.upload(
+          req.files.picture5.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures.push(imgUploaded5);
+      }
+    }
+
+    if (req.files.picture6) {
+      if (offer.product_pictures[5]) {
+        await cloudinary.uploader.destroy(offer.product_pictures[5].public_id);
+        const imgUploaded6 = await cloudinary.uploader.upload(
+          req.files.picture6.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures[5] = imgUploaded6;
+      } else {
+        const imgUploaded6 = await cloudinary.uploader.upload(
+          req.files.picture6.path,
+          { folder: `/vinted/offers/${offer.id}` }
+        );
+        offer.product_pictures.push(imgUploaded6);
+      }
+    }
+
+    offer.markModified("product_pictures");
+
     await offer.save();
     res.status(200).json({ message: "Offer updated." });
   } catch (error) {
@@ -100,7 +261,11 @@ router.put("/offer/update", isAuthenticated, async (req, res) => {
 router.delete("/offer/delete", isAuthenticated, async (req, res) => {
   try {
     offer = await Offer.findById(req.fields.id);
-    await cloudinary.uploader.destroy(offer.product_image.public_id);
+    // await cloudinary.uploader.destroy(offer.product_image.public_id);
+    const numberOfPictures = offer.product_pictures.length;
+    for (let i = 0; i < numberOfPictures; i++) {
+      await cloudinary.uploader.destroy(offer.product_pictures[i].public_id);
+    }
     await cloudinary.api.delete_folder(`/vinted/offers/${offer.id}`);
     await offer.remove();
     res.status(200).json({ message: "Deletion successfull" });
