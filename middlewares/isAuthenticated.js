80c61cd5580cd633ace1a5649b36c5c4e410b04f
Tom ROBERTO <tomroberto@Air-de-Tom.home>
2021-05-11 22:05:27 +0200

    isAuthenticated

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -9,10 +9,10 @@ const isAuthenticated = async (req, res, next) => {
         req.user = user;
         next();
       } else {
-        res.status(401).json({ message: "Unauthorized" });
+        res.status(401).json({ message: "Unauthorized, didnt find user" });
       }
     } else {
-      res.status(401).json({ message: "Unauthorized" });
+      res.status(401).json({ message: "Unauthorized, bad authorisation" });
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
